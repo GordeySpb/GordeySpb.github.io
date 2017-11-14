42005386b0f301de34dc14a5b20db957c1dd191f
Kirill Gordeew <gordey_89@mail.ru>
2017-11-14 16:30:54 +0300

    otherCommit

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
 let menu = (function() {
 	function showMenu() {
 		let menu = document.querySelector('.fullscreen-menu'),
-			buttonOpen = document.querySelector('.hamburger-menu');
+			buttonOpen = document.querySelector('.hamburger-menu'),
+			body = document.querySelector('body');
 
 		buttonOpen.addEventListener('click',function(){
 		menu.classList.add('isactive')
+		})
+
+		buttonOpen.addEventListener('click',function(){
+		body.classList.add('locked')
 		})
 
 	};
 
 	function closeMenu() {
 		let menu = document.querySelector('.fullscreen-menu'),
-			buttonClose = document.querySelector('.close-menu'); 
+			buttonClose = document.querySelector('.close-menu'),
+			body = document.querySelector('body'); 
 
 		buttonClose.addEventListener('click',function(){
 			menu.classList.remove('isactive')   
-		})   
+		})
+		
+		buttonClose.addEventListener('click',function(){
+		body.classList.remove('locked')
+		})
 	}
 	
 	return {
